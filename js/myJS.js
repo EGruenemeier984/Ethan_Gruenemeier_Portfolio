cd808c93346a20df3ac3c090dd7cc856ac73535f
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2018-11-26 13:13:18 -0700

    JavaScript update
    added more javascript to my website.

--- js/myJS.js
+++ js/myJS.js
@@ -1,22 +1,3 @@
-window.onscroll = function () {
-    scrollFunction()
-    stickyFunction()
-};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("topBtn").style.display = "block";
-    } else {
-        document.getElementById("topBtn").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 // This function is for the nav when the button is clicked the width is set to 250 pixels and the margin.
 // The nav button is also hidden when its pressed. 
 // If the nav is opened on mobile then it makes the whole screen the nav.
@@ -42,15 +23,57 @@ function closeNav() {
 var mobileNav = window.matchMedia("(max-width: 600px)");
 mobileNav.addListener(openNav);
 
-// These are the variables for the topnav and the offset of the top nav bar as it relates to the screen
-var navbar = document.getElementById("topnav");
-var sticky = document.body.scrollTop;
+// New Function for sticky navigation 
+// cache the element
+var $navBar = $('#topnav');
 
-// This is the function for the sticky nav bar that removes or adds the sticky class based on its y axis offset.
-function stickyFunction() {
-    if (window.sticky > 90 || document.documentElement.scrollTop > 90) {
-        navbar.classList.add("sticky");
-    } else if (window.sticky < 90 || document.documentElement.scrollTop > 90) {
-        navbar.classList.remove("sticky")
+// find original navigation bar position
+var navPos = $navBar.offset().top;
+
+// on scroll
+$(window).scroll(function () {
+
+    // get scroll position from top of the page
+    var scrollPos = $(this).scrollTop();
+
+    // check if scroll position is >= the nav position
+    if (scrollPos >= navPos) {
+        $navBar.addClass('sticky');
+    } else {
+        $navBar.removeClass('sticky');
     }
+
+});
+
+// ====================================================================================================
+//                                    Old Sticky Navigation Function 
+// These are the variables for the topnav and the offset of the top nav bar as it relates to the screen
+// var navbar = document.getElementById("topnav");
+// var sticky = document.body.scrollTop;
+// This is the function for the sticky nav bar that removes or adds the sticky class based on its y axis offset.
+// function stickyFunction() {
+// if (window.sticky > 90 || document.documentElement.scrollTop > 90) {
+// navbar.classList.add("sticky");
+//   } else if (window.sticky < 90 || document.documentElement.scrollTop > 90) {
+//       navbar.classList.remove("sticky")
+//   }
+// }
+// ====================================================================================================
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
