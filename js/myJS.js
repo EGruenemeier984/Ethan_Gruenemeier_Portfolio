c814002705e26c7569f661f2fc18e3f69833c5f6
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2018-12-07 15:16:53 -0700

    Added Google Maps API
    Added initial code for the API.

--- js/myJS.js
+++ js/myJS.js
@@ -77,3 +77,25 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Google Maps API
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    var uluru = {
+        lat: -25.344,
+        lng: 131.036
+    };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
